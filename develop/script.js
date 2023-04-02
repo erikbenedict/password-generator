4498256603d93b847f4cea515d332872b368d83f
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-04-01 20:10:51 -0400

    created TODO's, started assigning variables for user choice

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,16 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// TODO: Assign variables for user choices
+// * Alphabet Characters
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// * Numeric Characters
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// * Special Characters 
+var character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+
+
+//* Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +19,10 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// TODO: write function to generate password ^
+function generatePassword() {
+    
+}
+
+//* Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
