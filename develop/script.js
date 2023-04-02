97aa6fa88cd53a80c4f9400c85300e526475e793
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-04-01 22:37:40 -0400

    progress on function generatePassword

--- develop/script.js
+++ develop/script.js
@@ -1,23 +1,19 @@
-
-// TODO: Assign variables for user choices
-// * Alphabet Characters
+// * Array of users choices
+var choicePool = [];
+// * Lowercase Alphabet Characters
 var alphaLower = 'abcdefghijklmnopqrstuvwxyz';
 alphaLower = alphaLower.split('');
 // * Numeric Characters
 var number = '123456789';
 number = number.split('');
 // * Special Characters 
-var character = "!@#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-character = character.split('');
+var special = "!@#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+special = special.split('');
 // * Uppercase Alphabet Characters
 var upperCase = function (letters) {
     return letters.toUpperCase();
 };
 var alphaUpper = alphaLower.map(upperCase);
-
-//* Add event listener to generate button
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
 
 //* Write password to the #password input
 function writePassword() {
@@ -34,13 +30,44 @@ function generatePassword() {
 
     if (!charLength) {
         alert('This needs a value');
-    } else if (charLength < 8 || charLength > 128) {
-        charLength = prompt("You must choose")
-        
+    } else if (charLength < 8 || charLength > 128 || isNaN(charLength)) {
+        charLength = prompt("You must choose between 8-128 and it must be a number");
+        return;
     }
 
+    var confirmLowercase = confirm('Will this password contain lowercase characters?');
+    var confirmUppercase = confirm('Will this password contain uppercase characters?');
+    var confirmNumbers = confirm('Will this password contain numbers?');
+    var confirmSpecial = confirm('Will this password contain special characters?');
 
 
+    if (!confirmLowercase && !confirmUppercase && !confirmNumbers && !confirmSpecial) {
+        alert('You must choose one criteria');
+        generatePassword();
+    } 
 
+    if (confirmLowercase) {
+        choicePool = choicePool.concat(alphaLower);
+    }
+    if (confirmUppercase) {
+        choicePool = choicePool.concat(alphaUpper);
+    }
+    if (confirmNumbers) {
+        choicePool = choicePool.concat(number);
+    }
+    if (confirmSpecial) {
+        choicePool = choicePool.concat(special)
+    }
+
+    for (var i = 0; i < userInput; i++) { 
+        var randomIndex = Math.floor(Math.random() * choicePool.length);
+        var indexValue = choicePool[randomIndex];
+        password.push(indexValue);
+    }
+    return password.join("");
 
 }
+
+//* Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
